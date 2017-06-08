f3f9bf03ebdf6e9ea0c109525998bf0950379534
nilzxq <2013358717@qq.com>
2017-06-08 12:35:30 +0800

    first commit

---
+++ baike_spider/url_manager.py
@@ -0,0 +1,41 @@
+#coding:utf-8
+'''
+Created on 2017年5日28月
+
+@author: ZXQ20
+'''
+
+
+class UrlManager(object): 
+    def __init__(self): 
+        self.new_urls = set() 
+        self.old_urls = set() 
+    def add_new_url(self,url): 
+        if url is None: 
+            return None 
+        if url not in self.new_urls and url not in self.old_urls: 
+            self.new_urls.add(url) 
+    def add_new_urls(self,urls): 
+        if urls is None or len(urls)==0: 
+            return 
+        for url in urls: 
+            self.new_urls.add(url) 
+    def get_new_url(self): 
+        new_url = self.new_urls.pop() 
+        self.old_urls.add(new_url) 
+        return new_url 
+    def has_new_url(self): 
+        return len(self.new_urls)!=0
+
+
+  
+    
+    
+    
+    
+    
+    
+    
+
+
+
